fecb774475843de0037cc0ff42f4e35340fc0dc5
Lee Junho <dangercloz@gmail.com>
2017-08-12 00:16:05 +0900

    Basic notification feature

--- Example/GCNotificationView/ViewController.swift
+++ Example/GCNotificationView/ViewController.swift
@@ -10,16 +10,45 @@ import UIKit
 import GCNotificationView
 
 class ViewController: UIViewController {
-
+    @IBOutlet var customView: UIView!
+    
+    var yPoint: CGFloat = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        hideAllTabbar()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    func hideAllTabbar() {
+        self.tabBarController?.tabBar.isHidden = true
+        self.customView.isHidden = true
+    }
 }
 
+extension ViewController {
+    @IBAction func toggleYAxis(_ sender: UISegmentedControl) {
+        hideAllTabbar()
+        
+        switch sender.selectedSegmentIndex {
+        case 0:
+            yPoint = 0
+        case 1:
+            yPoint = self.tabBarController?.tabBar.bounds.height ?? 0
+            self.tabBarController?.tabBar.isHidden = false
+        case 2:
+            yPoint = self.customView.bounds.height
+            self.customView.isHidden = false
+        default:
+            break
+        }
+    }
+    
+    @IBAction func toggleShowNotification(_ sender: UIButton) {
+        GCNotificationView(yPoint: yPoint).show(message: "Hello, World!")
+    }
+}
